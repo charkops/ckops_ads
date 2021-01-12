470518a620d9f260ed36f76f8f0191853fa79eb3
Charis Kopsacheilis <charis.kopsacheilis@gmail.com>
2021-01-12 11:22:44 +0200

    Add main plugin file

---
+++ ckops_ads.php
@@ -0,0 +1,15 @@
+<?php
+  /*
+    Plugin Name: Ckops Ads
+    Plugin URI: https://github.com/charkops/ckops_ads
+    description: A Plugin that inserts a Google Test Add after 2nd paragraph of each post
+    Version: 1.0
+    Author: Charis Kopsacheilis
+    Author URI: https://github.com/charkops
+    License: GPL2
+  */
+
+  
+
+
+?>
